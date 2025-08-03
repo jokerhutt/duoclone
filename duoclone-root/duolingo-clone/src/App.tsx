cb40947ab530a5cccf2748de04a2d05f85afe800
Dave <davidglogowski2003@gmail.com>
2025-08-04 01:21:07 +0200

    Feat: add header and tailwind dependencies

--- duoclone-root/duolingo-clone/src/App.tsx
+++ duoclone-root/duolingo-clone/src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
 
+
 function App() {
 
   return (
     <>
-      <div className='w-full h-full bg-red-500'>
-        <p className='text-blue-400'>Hello world</p>
+      <div className='w-dvw h-dvh flex flex-col bg-duoBackground'>
+        <nav className='w-full flex justify-between items-center h-14 px-2'>
+          <img src="/French_flag.webp" className='w-fit h-10'/>
+          <img src="/streak-flame-updated-v0-3n46sx7a0e9b1.png" className='w-fit h-11' />
+          <img src="https://d35aaqx5ub95lt.cloudfront.net/vendor/45c14e05be9c1af1d7d0b54c6eed7eee.svg" className='w-10 h-9'/>
+          <img src="https://d35aaqx5ub95lt.cloudfront.net/images/hearts/8fdba477c56a8eeb23f0f7e67fdec6d9.svg" className='w-10 h-12'/>
+          
+        </nav>
+
       </div>
     </>
   )
