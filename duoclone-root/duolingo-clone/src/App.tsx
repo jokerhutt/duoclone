a7e66c11f391d517e98ce5e53a67d15f74094a06
Dave <davidglogowski2003@gmail.com>
2025-08-04 02:01:54 +0200

    Style: add button click animation

--- duoclone-root/duolingo-clone/src/App.tsx
+++ duoclone-root/duolingo-clone/src/App.tsx
@@ -11,7 +11,8 @@ function App() {
         <LearnHeader/>
 
         <div className='px-4 w-full'>
-          <div className='w-full h-20 flex bg-duoGreen rounded-2xl shadow-duoShadow'>
+          <button className='w-full h-20 active:translate-y-[5px] active:shadow-none shadow-duoShadow bg-duoGreen rounded-2xl'>
+            <div className='flex rounded-2xl h-20  w-full'>
             <div className='w-5/6 h-full px-4 pb-3 flex flex-col'>
               <div className='mt-3 text-duoSubText'>
                 <p>SECTION 3, UNIT 1</p>
@@ -23,7 +24,9 @@ function App() {
             <div className='h-full w-1/6 border-l flex justify-center items-center'>
               <img src='https://d35aaqx5ub95lt.cloudfront.net/images/path/5b531828e59ae83aadb3d88e6b3a98a8.svg'/>
             </div>
-          </div>
+            </div>
+
+          </button>
 
         </div>
 
@@ -33,3 +36,5 @@ function App() {
 }
 
 export default App
+
+//    --transform-buttonTransform: translateY(5px);
