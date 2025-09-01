4332d6be97bf442701fabd4813f5e4ad27a029c2
Dave <davidglogowski2003@gmail.com>
2025-09-01 21:32:08 +0200

    Add API Paths and Path Parsers

---
+++ duoclone-root/duolingo-clone/src/util/paths.ts
@@ -0,0 +1,18 @@
+
+export const API_URL = "http://localhost:"
+export const API_PORT = "8080"
+export const API_PREFIX = "/api"
+
+export const API_PATH = API_URL + API_PORT + API_PREFIX
+
+export const GET_LESSONS = (unitId: number) => API_PATH + `/units/${unitId}/lessons`;
+
+//RETURN IDS AS ARRAY, USE FOR PARENTS
+export const GET_UNIT_IDS = (sectionId: number) => API_PATH + `/courses/${sectionId}/units/ids`
+export const GET_LESSON_IDS = (unitId: number) => API_PATH + `/units/${unitId}/lessons/ids`
+
+
+//RETURN FULL OBJECTS AS ARRAYS, USE FOR BATCHER
+export const GET_UNITS_FROM_IDS = (unitIds: string) => API_PATH + `/units/ids?${unitIds}`
+export const GET_LESSONS_FROM_IDS = (lessonIds: string) => API_PATH + `/lessons/ids?${lessonIds}`
+
