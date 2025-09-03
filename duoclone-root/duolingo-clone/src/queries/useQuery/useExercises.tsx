c2cae1b39e9f372b645edadb42ab96ff2242d91f
Dave <davidglogowski2003@gmail.com>
2025-09-03 23:46:08 +0200

    Fix exercises query

--- duoclone-root/duolingo-clone/src/queries/useQuery/useExercises.tsx
+++ duoclone-root/duolingo-clone/src/queries/useQuery/useExercises.tsx
@@ -7,11 +7,11 @@ import { fetchLessonsByUnit } from "./useLessonByUnit";
 export function useExercises(lessonId: number) {
   return useQuery({
     queryKey: qk.exercises(lessonId),
-    queryFn: () => fetchLessonsByUnit(lessonId),
+    queryFn: () => fetchExercisesByLesson(lessonId),
   });
 }
 
-export async function getExercises(lessonId: number): Promise<Exercise[]> {
+export async function fetchExercisesByLesson(lessonId: number): Promise<Exercise[]> {
   const res = await fetch(GET_EXERCISES_BY_LESSON(lessonId));
   if (!res.ok) throw new Error("Failed to fetch exercises");
   return (await res.json()) as Exercise[];
