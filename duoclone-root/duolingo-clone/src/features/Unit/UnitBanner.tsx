3a0c2c8d45a82f6a93785316dde40a92de03e248
Dave <davidglogowski2003@gmail.com>
2025-09-13 16:37:12 +0200

    Clean up unit data variables

--- duoclone-root/duolingo-clone/src/features/Unit/UnitBanner.tsx
+++ duoclone-root/duolingo-clone/src/features/Unit/UnitBanner.tsx
@@ -1,8 +1,5 @@
-import { useEffect } from "react";
 import { RectangleButton } from "../../components/atoms/Button/RectangleButton";
-import { GET_LESSONS_BY_UNIT } from "../../util/paths";
 import type { UnitType } from "../../Types/UnitType";
-import { colorMap } from "../../util/colorMap";
 
 type UnitBannerProps = {
   currentUnit: UnitType | null;
@@ -10,23 +7,15 @@ type UnitBannerProps = {
 
 export function UnitBanner({currentUnit}: UnitBannerProps){
 
-  const unitTile = currentUnit?.title;
-  const unitNumber =
-    unitTile == "Discuss a new Job"
-      ? 1
-      : unitTile == "Pack for a Vacation"
-      ? 3
-      : 2;
-
   return (
     <RectangleButton unitColor={currentUnit?.color}>
       <div className="flex rounded-2xl h-20 w-full">
         <div className="w-5/6 h-full px-4 pb-3 flex flex-col">
           <div className="mt-3 text-duoSubText">
-            <p>SECTION 3, UNIT {unitNumber}</p>
+            <p>SECTION 3, UNIT {currentUnit?.orderIndex}</p>
           </div>
           <div className="text-white text-xl">
-            <p>{unitTile}</p>
+            <p>{currentUnit?.title}</p>
           </div>
         </div>
         <div className="h-full w-1/6 border-l flex justify-center items-center">
