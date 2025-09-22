97bd53ada60e66379e745910d8c8519748229f41
Dave <davidglogowski2003@gmail.com>
2025-09-22 23:56:01 +0200

    Add currentUser query to sectionPage

--- duoclone-root/duolingo-clone/src/features/Section/SectionPage.tsx
+++ duoclone-root/duolingo-clone/src/features/Section/SectionPage.tsx
@@ -11,6 +11,7 @@ import { useCourseProgress } from "../../queries/useQuery/useCourseProgress";
 import { SpinnerPage } from "./SpinnerPage";
 import { MainFooter } from "../Common/MainFooter";
 import type { UnitType } from "../../Types/UnitType";
+import { useCurrentUser } from "../../queries/useQuery/useCurrentUser";
 
 export function SectionPage() {
   const { isLoading, isError } = useSectionTree(1);
@@ -21,10 +22,12 @@ export function SectionPage() {
   const [currentUnit, setCurrentUnit] = useState<UnitType | null>(null);
   const unitRefs = useRef<(HTMLElement | null)[]>([]);
 
+  const {data: currentUser, isLoading: loadingUser} = useCurrentUser(1);
+
 useUnitObserver(unitRefs, units ?? [], (u) => setCurrentUnit(u));
 
   if (isError) return <SpinnerPage color="border-red-400" />;
-  if (isLoading || !units || !courseProgress) return <SpinnerPage />;
+  if (loadingUser || isLoading || !units || !courseProgress) return <SpinnerPage />;
 
   return (
     <>
