08894a003cafbac9a328d6d84c0a1f397e7f1375
Dave <davidglogowski2003@gmail.com>
2025-09-24 18:29:05 +0200

    Make main footer persist between pages by adding main layout wrapper

--- duoclone-root/duolingo-clone/src/App.tsx
+++ duoclone-root/duolingo-clone/src/App.tsx
@@ -3,13 +3,19 @@ import { SectionPage } from "./features/Section/SectionPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { LessonPage } from "./features/Lesson/LessonPage";
 import { LessonCompletePage } from "./features/Lesson/LessonCompletePage";
+import { QuestsPage } from "./features/Quests/QuestsPage";
+import { MainLayout } from "./components/layouts/MainLayout";
 
 function App() {
   return (
     <Router>
       <div className="w-dvw h-dvh flex flex-col overflow-auto bg-duoBackground">
         <Routes>
-          <Route path="" element={<SectionPage />} />
+          <Route element={<MainLayout />}>
+            <Route path="" element={<SectionPage />} />
+            <Route path="/quests" element={<QuestsPage />} />
+          </Route>
+
           <Route path="/lessons/:lessonId/:position" element={<LessonPage />} />
           <Route
             path="/lessons/:lessonId/complete"
