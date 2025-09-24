85417d972c4f669ce0345fc37470f8894ec21c19
Dave <davidglogowski2003@gmail.com>
2025-09-24 19:37:49 +0200

    Move widgets to own component

---
+++ duoclone-root/duolingo-clone/src/components/atoms/Widget/ContentWidget.tsx
@@ -0,0 +1,15 @@
+type ContentWidgetProps = {
+  title?: string;
+  children: React.ReactNode;
+};
+
+export function ContentWidget({ title, children }: ContentWidgetProps) {
+  return (
+    <div className="w-full flex flex-col gap-4">
+      {title && <h2 className="text-2xl text-white">{title}</h2>}
+      <div className=" rounded-2xl p-4 border-2 h-full w-full border-duoGrayBorder">
+        {children}
+      </div>
+    </div>
+  );
+}
--- duoclone-root/duolingo-clone/src/features/Quests/QuestsPage.tsx
+++ duoclone-root/duolingo-clone/src/features/Quests/QuestsPage.tsx
@@ -1,36 +1,31 @@
 import { StreakIcon } from "../../components/atoms/Icons/StreakIcon";
+import { ContentWidget } from "../../components/atoms/Widget/ContentWidget";
 import { MainFooter } from "../Common/MainFooter";
 
-export function QuestsPage () {
-
-    return (
-        <>
-            <div className="w-full p-4 flex flex-col gap-4">
-                <h2 className="text-2xl text-white">Daily Quests</h2>
-                <div className=" rounded-2xl p-4 border-2 h-full w-full border-duoGrayBorder">
-                    <div className="w-hull flex flex-col">
-                        <div className="w-full flex items-center">
-
-                            <div className="w-20">
-                                <StreakIcon height="h-12"/>
-                            </div>
-
-                            <div className="flex flex-col w-full justify-between gap-3">
-                                <div className="w-full text-white text-xl">
-                                    <p>Extend your streak</p>
-                                </div>
-                                <div className="w-4/5 rounded-full bg-duoGrayLocked h-4">
-
-                                </div>
-                            </div>
-                            
-
-                        </div>
-                    </div>
-                </div>
+export function QuestsPage() {
+  return (
+    <div className="w-full p-4">
+      <ContentWidget>
+        <div className="w-hull flex flex-col">
+          <div className="w-full flex items-center">
+            <div className="w-20">
+              <StreakIcon height="h-12" />
             </div>
-        </>
-    )
 
-
+            <div className="flex flex-col w-full justify-between gap-3">
+              <div className="w-full text-white text-xl">
+                <p>Extend your streak</p>
+              </div>
+              <div className="w-4/5 relative rounded-full bg-duoGrayLocked h-4">
+                <img
+                  className="absolute right-0 center bottom-0"
+                  src="https://d35aaqx5ub95lt.cloudfront.net/images/goals/df7eda7cc1cc833ba30cd1e82781b68f.svg"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </ContentWidget>
+    </div>
+  );
 }
