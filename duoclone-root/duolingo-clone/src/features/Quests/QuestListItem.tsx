c1e1a1e4814dd4e634ab185afc4202ec476c8775
Dave <davidglogowski2003@gmail.com>
2025-09-24 22:23:38 +0200

    Add props data to quest list item component, add quest metadata functions

--- duoclone-root/duolingo-clone/src/features/Quests/QuestListItem.tsx
+++ duoclone-root/duolingo-clone/src/features/Quests/QuestListItem.tsx
@@ -1,24 +1,33 @@
 import { StreakIcon } from "../../components/atoms/Icons/StreakIcon";
+import { QUEST_METADATA } from "../../Types/QuestCategory";
+import type { QuestType } from "../../Types/QuestType";
 
 type QuestListItemProps = {
     isLast?: boolean;
+    quest: QuestType;
 };
 
-export function QuestListItem({isLast}: QuestListItemProps) {
+export function QuestListItem({isLast, quest}: QuestListItemProps) {
+
+  const total = quest.total;
+  const completed = quest.progress;
 
   const showBottomBorder = true ? "border-b-duoGrayBorder border-b" : ""
 
+  const metaData = QUEST_METADATA[quest.code];
+
   return (
-    <div className={`w-full flex items-center py-8 ${showBottomBorder}`}>
+    <div className={`w-full flex items-center py-7 ${showBottomBorder}`}>
       <div className="w-20">
-        <StreakIcon height="h-12" />
+        <img className="h-16" src={metaData.iconUrl}/>
       </div>
 
-      <div className="flex flex-col w-full justify-between gap-3">
+      <div className="flex flex-col w-3/4 justify-between gap-3">
         <div className="w-full text-white text-xl">
-          <p>Extend your streak</p>
+          <p>{metaData.description}</p>
         </div>
-        <div className="w-4/5 relative rounded-full bg-duoGrayLocked h-4">
+        <div className="w-full relative rounded-full flex items-center justify-center bg-duoGrayLocked h-5">
+          <p className="text-duoLightGray mr-5">{completed} / {total}</p>
           <img
             className="absolute right-0 center bottom-0"
             src="https://d35aaqx5ub95lt.cloudfront.net/images/goals/df7eda7cc1cc833ba30cd1e82781b68f.svg"
---
+++ duoclone-root/duolingo-clone/src/features/Quests/QuestsHeader.tsx
@@ -0,0 +1,17 @@
+import { Header } from "../../components/molecules/Header/Header";
+
+export function QuestsHeader () {
+
+    return (
+
+        <Header padding="4px">
+
+            <div>
+
+            </div>
+
+        </Header>
+
+    )
+
+}
