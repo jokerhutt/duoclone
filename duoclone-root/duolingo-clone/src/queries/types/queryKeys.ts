7cae634c391a92532a11c1c1e89adaf4ef91ddef
Dave <davidglogowski2003@gmail.com>
2025-09-24 21:48:48 +0200

    Remove duplicate querykey file

--- duoclone-root/duolingo-clone/src/queries/types/queryKeys.ts
+++ duoclone-root/duolingo-clone/src/queries/types/queryKeys.ts
@@ -2,11 +2,11 @@ export const qk = {
   section: (id: number) => ["section", id] as const,
   unitsBySection: (id: number) => ["sections", id, "units"] as const,
   unit: (id: number) => ["unit", id] as const,
-  lessonsByUnit: (id: number, userId: number) =>
-    ["units", id, "lessons"] as const,
+  lessonsByUnit: (id: number) => ["units", id, "lessons"] as const,
   lesson: (id: number) => ["lesson", id] as const,
   sectionTree: (id: number) => ["sectionTree", id] as const,
   courseProgress: (id: number) => ["courseProgress", id] as const,
   exercises: (lessonId: number) => ["exercises", lessonId] as const,
   user: (id: number) => ["users", id] as const,
+    quests: (id: number) => ["quests", id] as const,
 };
