a09c63f790ece61102336019ebb32a0a5c827a51
Dave <davidglogowski2003@gmail.com>
2025-09-24 22:09:15 +0200

    Replace mock quests with useQuests query data

--- duoclone-root/duolingo-clone/src/features/Quests/QuestListWidget.tsx
+++ duoclone-root/duolingo-clone/src/features/Quests/QuestListWidget.tsx
@@ -1,16 +1,22 @@
+import { Spinner } from "../../components/atoms/Loading/Spinner";
+import { useQuests } from "../../queries/useQuery/useQuests";
+import { SpinnerPage } from "../Section/SpinnerPage";
 import { QuestListItem } from "./QuestListItem";
 
 type QuestListWidgetProps = {};
 
 export function QuestListWidget({}: QuestListWidgetProps) {
-  const mocks = [1, 2, 3, 4];
 
-  const isLast = (index: number) => index == (mocks.length - 1);
+  const {data: quests, isLoading: isLoading, isError: isError} = useQuests(1);  
+
+  if (!quests || isLoading || isError) return <SpinnerPage/>
+
+  const isLast = (index: number) => index == (quests.length - 1);
 
   return (
   <div className="w-hull flex flex-col gap-2">
-    {mocks.map((mock, index) => (
-        <QuestListItem isLast={isLast(index)}/>
+    {quests.map((quest, index) => (
+        <QuestListItem quest={quest} isLast={isLast(index)}/>
     ))}
   </div>)
 }
