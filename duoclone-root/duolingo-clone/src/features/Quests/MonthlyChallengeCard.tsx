2c62c15b38f75a9feb6735be6ef7be800d479320
Dave <davidglogowski2003@gmail.com>
2025-09-24 23:37:49 +0200

    Add progress bar and style to MonthlyChallengeCard

--- duoclone-root/duolingo-clone/src/features/Quests/MonthlyChallengeCard.tsx
+++ duoclone-root/duolingo-clone/src/features/Quests/MonthlyChallengeCard.tsx
@@ -1,17 +1,19 @@
+import { MonthlyChallengeProgressBar } from "./MonthlyChallengeProgressBar";
 
 export function MonthlyChallengeCard () {
 
+
     return (
-        <div className="w-full p-6 flex gap-4 flex-col bg-duoDarkGreen">
+        <div className="w-full p-4 flex gap-4 flex-col bg-duoDarkGreen">
 
             <div className="flex w-full">
                 <div className="bg-white py-1 px-2 rounded-lg">
                     <p className="text-duoDarkGreen">SEPTEMBER</p>
                 </div>
             </div>
-            <div className="w-full h-20 flex flex-col p-4 bg-duoBackground rounded-xl">
+            <div className="w-full flex flex-col gap-2 p-4 bg-duoBackground rounded-xl">
                 <p className="text-white text-lg">Complete 20 quests</p>
-
+                <MonthlyChallengeProgressBar completed={5} total={20}/>
             </div>
 
         </div>
---
+++ duoclone-root/duolingo-clone/src/features/Quests/MonthlyChallengeProgressBar.tsx
@@ -0,0 +1,27 @@
+import { ProgressBar } from "../../components/atoms/Bar/ProgressBar";
+
+type MonthlyChallengeProgressBarProps = {
+  completed: number;
+  total: number;
+};
+
+export function MonthlyChallengeProgressBar({
+  completed,
+  total,
+}: MonthlyChallengeProgressBarProps) {
+  const chestIcon =
+    "https://d35aaqx5ub95lt.cloudfront.net/images/58175d43db9f94dc1d83cd35d63ef58d.svg";
+ 
+  const bgcolor = "text-duoBackground bg-duoDarkGreen"
+
+  return (
+    <ProgressBar
+      completed={completed}
+      total={total}
+      barColor={bgcolor}
+      showGoldOnComplete={true}
+      showCountText={true}
+      icon={chestIcon}
+    />
+  )
+}
