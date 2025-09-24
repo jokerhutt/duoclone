ed48457d68b53d22ef1a69f9e4c74b8fed1527d0
Dave <davidglogowski2003@gmail.com>
2025-09-24 23:04:34 +0200

    Add invalidation of quest queries when lesson complete

--- duoclone-root/duolingo-clone/src/queries/mutations/useLessonComplete.tsx
+++ duoclone-root/duolingo-clone/src/queries/mutations/useLessonComplete.tsx
@@ -27,6 +27,7 @@ export const useLessonComplete = ({ lessonId }: useLessonCompleteParams) => {
       return res.json();
     },
     onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: qk.quests(1) });
       queryClient.setQueryData(qk.lesson(data.lessonId), data.updatedLesson);
       queryClient.setQueryData(
         qk.courseProgress(1),
