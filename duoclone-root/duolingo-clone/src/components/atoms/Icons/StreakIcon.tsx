cde808b842cdb834dd75fb312433c2799b49bca8
Dave <davidglogowski2003@gmail.com>
2025-09-24 19:28:46 +0200

    Fill Quests Page with some mock data

--- duoclone-root/duolingo-clone/src/components/atoms/Icons/StreakIcon.tsx
+++ duoclone-root/duolingo-clone/src/components/atoms/Icons/StreakIcon.tsx
@@ -1,9 +1,14 @@
-export function StreakIcon() {
+
+type StreakIconProps = {
+  height?: string;
+};
+
+export function StreakIcon({height = "h-8"}: StreakIconProps) {
   return (
     <>
       <img
         src="https://d35aaqx5ub95lt.cloudfront.net/vendor/398e4298a3b39ce566050e5c041949ef.svg"
-        className="w-fit h-8"
+        className={`w-fit ${height}`}
       />
     </>
   );
--- duoclone-root/duolingo-clone/src/features/Quests/QuestsPage.tsx
+++ duoclone-root/duolingo-clone/src/features/Quests/QuestsPage.tsx
@@ -1,9 +1,34 @@
+import { StreakIcon } from "../../components/atoms/Icons/StreakIcon";
 import { MainFooter } from "../Common/MainFooter";
 
 export function QuestsPage () {
 
     return (
         <>
+            <div className="w-full p-4 flex flex-col gap-4">
+                <h2 className="text-2xl text-white">Daily Quests</h2>
+                <div className=" rounded-2xl p-4 border-2 h-full w-full border-duoGrayBorder">
+                    <div className="w-hull flex flex-col">
+                        <div className="w-full flex items-center">
+
+                            <div className="w-20">
+                                <StreakIcon height="h-12"/>
+                            </div>
+
+                            <div className="flex flex-col w-full justify-between gap-3">
+                                <div className="w-full text-white text-xl">
+                                    <p>Extend your streak</p>
+                                </div>
+                                <div className="w-4/5 rounded-full bg-duoGrayLocked h-4">
+
+                                </div>
+                            </div>
+                            
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 
