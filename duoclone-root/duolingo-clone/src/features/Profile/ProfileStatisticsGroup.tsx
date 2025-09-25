1928a80f8f3808b976f51581b88a9dfd9733a236
Dave <davidglogowski2003@gmail.com>
2025-09-25 19:29:03 +0200

    Add props to ProfileStatsWidget

--- duoclone-root/duolingo-clone/src/features/Profile/ProfileStatisticsGroup.tsx
+++ duoclone-root/duolingo-clone/src/features/Profile/ProfileStatisticsGroup.tsx
@@ -1,14 +1,16 @@
 import { ContentWidget } from "../../components/atoms/Widget/ContentWidget";
 import { ProfileStatsWidget } from "./ProfileStatsWidget";
 
-
 export function ProfileStatisticsGroup() {
+  const xpIcon = "/icon-images/fittedXpImg.svg";
+  const streakIcon = "/icon-images/fittedStreakImg.svg";
+
   return (
     <div className="flex w-full flex-col gap-2 px-4">
       <p className="text-white text-2xl">Statistics</p>
       <div className="w-full flex gap-4 justify-between">
-        <ProfileStatsWidget />
-        <ProfileStatsWidget />
+        <ProfileStatsWidget iconPath={streakIcon} statDescription="Day Streak" count={3}/>
+        <ProfileStatsWidget iconPath={xpIcon} statDescription="Total XP" count={3}/>
       </div>
     </div>
   );
--- duoclone-root/duolingo-clone/src/features/Profile/ProfileStatsWidget.tsx
+++ duoclone-root/duolingo-clone/src/features/Profile/ProfileStatsWidget.tsx
@@ -6,16 +6,19 @@ type ProfileStatsWidgetProps = {
     count: number;
 }
 
-export function ProfileStatsWidget() {
+export function ProfileStatsWidget({iconPath, statDescription, count}: ProfileStatsWidgetProps) {
+
+
+
   return (
     <ContentWidget padding="p-2">
       <div className="w-full flex gap-2">
         <div className="w-10 flex justify-center">
-          <img className="h-6" src="/icon-images/fittedStreakImg.svg" />
+          <img className="h-6" src={iconPath} />
         </div>
         <div className="w-full flex flex-col">
-          <p className="text-white text-lg">1234</p>
-          <p className="text-duoGrayButtonText">Day Streak</p>
+          <p className="text-white text-lg">{count}</p>
+          <p className="text-duoGrayButtonText">{statDescription}</p>
         </div>
       </div>
     </ContentWidget>
