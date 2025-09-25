c83c722e0f327f05e50caef3fc08e899a5f18180
Dave <davidglogowski2003@gmail.com>
2025-09-25 18:47:19 +0200

    Add some mock images

--- duoclone-root/duolingo-clone/src/features/Profile/ProfilePage.tsx
+++ duoclone-root/duolingo-clone/src/features/Profile/ProfilePage.tsx
@@ -1,13 +1,15 @@
 import { ProfileHeader } from "./ProfileHeader";
 
-export function ProfilePage () {
-
-    return (
-        <>
-            <ProfileHeader/> 
-
-        
-        </>
-    )
-
+export function ProfilePage() {
+  return (
+    <div className="w-full h-full flex flex-col items-center">
+      <ProfileHeader />
+      <div className="mt-30 flex px-6 justify-center">
+        <img 
+          className="w-full h-80 object-cover rounded-3xl" 
+          src="/pfp/av3.png" 
+        />
+      </div>
+    </div>
+  );
 }
