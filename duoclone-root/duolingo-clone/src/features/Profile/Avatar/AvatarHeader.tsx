77047daad407946b5e4b65a5886bde204d98ea16
Dave <davidglogowski2003@gmail.com>
2025-09-29 00:46:24 +0200

    Add close page button navigation to avatar header

--- duoclone-root/duolingo-clone/src/features/Profile/Avatar/AvatarHeader.tsx
+++ duoclone-root/duolingo-clone/src/features/Profile/Avatar/AvatarHeader.tsx
@@ -18,7 +18,7 @@ export function AvatarHeader ({currentUserId, submit}: AvatarHeaderProps) {
 
         <Header>
             <div className="w-full border-b-2 border-b-duoGrayButtonText h-full py-6 px-4 flex justify-between items-center">
-                <p className="text-lg text-start text-duoGrayButtonText w-20">X</p>
+                <p onClick={() => closePage()} className="text-lg text-start text-duoGrayButtonText w-20">X</p>
                 <p className="text-xl text-center text-duoGrayButtonText w-full">Edit Avatar</p>
                 <p onClick={submit} className="w-20 text-end text-duoBlue text-xl">DONE</p>
             </div>
