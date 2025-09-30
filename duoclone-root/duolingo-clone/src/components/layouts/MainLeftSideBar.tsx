438ffd190f5bfe222d6bb26e2e7986d200a1d234
Dave <davidglogowski2003@gmail.com>
2025-09-30 18:34:46 +0200

    Add nav buttons to Left Sidebar

--- duoclone-root/duolingo-clone/src/components/layouts/MainLeftSideBar.tsx
+++ duoclone-root/duolingo-clone/src/components/layouts/MainLeftSideBar.tsx
@@ -1,5 +1,17 @@
+import { MainNavigationButtons } from "../../features/Common/MainNavigationButtons";
+import { useCurrentUser } from "../../queries/useQuery/Auth/useCurrentUser";
+
 export function MainLeftSidebar() {
+
+  const {data: currentUser} = useCurrentUser();  
+
   return (
-    <aside className="hidden lg:block border-r border-duoGrayBorder bg-duoBackground w-80 2xl:w-100" />
+    <aside className="hidden border-r border-duoGrayBorder p-2 lg:flex flex-col bg-duoBackground xl:w-80 lg:w-60 2xl:w-85"> 
+    
+        <div className="flex p-4 flex-col w-full h-full">
+            <MainNavigationButtons currentUser={currentUser}/>
+        </div>
+    
+    </aside>
   );
 }
